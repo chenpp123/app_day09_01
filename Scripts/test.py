c58f0d14b7795c13c42980398d8c7bdfac21410d
chenpp123 <chenpp123>
2018-07-10 15:34:18 +0800

    ceshi

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,14 +1,15 @@
 import allure
-
+import pytest
 
 class Test_aa:
+
+    def get_screen(self):
+        with open("./Screen/test.png","rb") as f:
+            allure.attach("屏幕截图",f.read(),allure.attach_type.PNG)
 
     def test_abc(self):
         # 添加描述
         allure.attach("描述","描述内容")
 
-        with open("./Screen/test.png","rb") as f:
-            allure.attach("屏幕截图",f.read(),allure.attach_type.PNG)
-            
-        assert 0,"hahahahha"
+        assert 0,self.get_screen()
 
